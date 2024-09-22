e59898eaf6301e2722aa5a0e15097504376d3451
kurtisha <kurtishvilii@gmail.com>
2024-09-22 15:38:42 +0400

    finish pagination module

--- Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
+++ Restaurants.Application/Restaurants/Dtos/RestaurantDto.cs
@@ -6,19 +6,19 @@ namespace Restaurants.Application.Restaurants.Dtos
     {
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name { get; set; } = default!;
 
-        public string Description { get; set; }
+        public string Description { get; set; } = default!;
 
-        public string Category { get; set; }
+        public string Category { get; set; } = default!;
 
         public bool HasDelivery { get; set; }
 
-        public string City { get; set; }
+        public string? City { get; set; }
 
-        public string Street { get; set; }
+        public string? Street { get; set; }
 
-        public string PostalCode { get; set; }
+        public string? PostalCode { get; set; }
 
         public List<DishDto> Dishes { get; set; } = [];
     }
--- Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
+++ Restaurants.Infrastructure/Repositories/RestaurantsRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Constants;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Persistence;
+using System.Linq.Expressions;
 
 namespace Restaurants.Infrastructure.Repositories
 {
@@ -28,6 +30,40 @@ namespace Restaurants.Infrastructure.Repositories
             var restaurants = await dbContext.Restaurants.ToListAsync();
 
             return restaurants;
+        }
+
+        public async Task<(IEnumerable<Restaurant>, int)> GetAllMatchingAsync(string? searchPhrase, int pageSize, int pageNumber, string? sortBy, SortDirection sortDirection)
+        {
+            var searchPhraseLower = searchPhrase?.ToLower();
+
+            var baseQuery = dbContext.Restaurants
+                .Where(r => searchPhrase == null || (r.Name.ToLower().Contains(searchPhraseLower)
+                                                 || r.Description.ToLower().Contains(searchPhraseLower)));
+
+            var totalCount = await baseQuery.CountAsync();
+
+            if (sortBy != null)
+            {
+                var columnsSelector = new Dictionary<string, Expression<Func<Restaurant, object>>>
+                {
+                    {nameof(Restaurant.Name), r => r.Name },
+                    {nameof(Restaurant.Category), r => r.Category },
+                    {nameof(Restaurant.Description), r => r.Description }
+                };
+
+                var seelctedColumn = columnsSelector[sortBy];
+
+                baseQuery = sortDirection == SortDirection.Ascending 
+                    ? baseQuery.OrderBy(seelctedColumn) 
+                    : baseQuery.OrderByDescending(seelctedColumn);
+            }
+
+            var restaurants = await baseQuery
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .ToListAsync();
+
+            return (restaurants, totalCount);
         }
 
         public async Task<Restaurant?> GetByIdAsync(int id, CancellationToken cancellationToken)
