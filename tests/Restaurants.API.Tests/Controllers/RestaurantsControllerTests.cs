57a0e8fc68a076e380931699ce690b191902123b
kurtisha <kurtishvilii@gmail.com>
2024-10-06 18:14:09 +0400

    fix tests

--- tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -8,6 +8,7 @@ using Moq;
 using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Seeders;
 using System.Net;
 using System.Net.Http.Json;
 using Xunit;
@@ -18,6 +19,7 @@ public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Pr
 {
     private readonly WebApplicationFactory<Program> _factory;
     private readonly Mock<IRestaurantsRepository> _restaurantsRepositoryMock = new();
+    private readonly Mock<IRestaurantSeeder> _restaurantSeederMock = new();
 
     public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
     {
@@ -27,6 +29,8 @@ public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Pr
             {
                 services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
                 services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantsRepository), _ => _restaurantsRepositoryMock.Object));
+
+                services.Replace(ServiceDescriptor.Scoped(typeof(IRestaurantSeeder), _ => _restaurantSeederMock.Object));
             });
         });
     }
